a7b8771f14b4f363e1857750c25db74ae57b90fa
255cloudy <collorucha@gmail.com>
2022-06-23 14:07:52 +0300

    carousel fade animation perfected

--- index.js
+++ index.js
@@ -36,3 +36,66 @@ function showMenu(){
     menuTrigger.classList.add('logo-hidden');
     closeIcon.classList.add('close-icon-open');
 }
+class Carousel {
+    constructor(carouselNode) {
+        this.currItem = 0;
+        this.currTime = 0;
+        this.itemSelected = 'carousel-item-selected'
+        this.buttonSelected = 'carousel-button-selected'
+        this.carouselImage = '.carousel-image'
+        this.carousel = carouselNode
+
+        this.items = this.carousel.querySelectorAll('.carousel-item');
+        this.items.forEach(item=> {
+            item.querySelector(this.carouselImage).classList.add('transparent')
+        })
+        this.items[this.currItem].querySelector(this.carouselImage).classList.add('opaque')
+        this.btns = Array.from(this.items, () => {
+            const node = document.createElement('span')
+            node.classList.add('carousel-btn')
+            console.log(node)
+            return node
+        })
+        console.log(this.btns)
+        let btnContainer = document.createElement('div')
+        btnContainer.classList.add('carousel-nav')
+        btnContainer.append(...this.btns)
+        this.carousel.append(btnContainer)
+        this.btns[this.currItem].classList.add(this.buttonSelected)
+        this.btns.forEach((btn, index) => {
+            console.log('setting listener')
+            btn.addEventListener('click',()=> {
+                this.imageFade(index)
+            })})
+    }
+    updateCarousel(index){
+        this.btns[this.currItem].classList.remove(this.buttonSelected)
+        this.btns[index].classList.add(this.buttonSelected)
+        this.currItem = index
+    }
+    fadeAnimation(index){
+        let fadeIn = [
+            {background: 'red'},
+            // {opacity: 1}
+        ]
+        let fadeInTiming = {
+            fill: 'forwards',
+            duration: 3000,
+            iterations: 1,
+        }
+        console.log(this.items[index])
+        this.items[index].animate(fadeIn, fadeInTiming)
+        this.updateCarousel(index)
+    }
+    imageFade(index){
+        // console.log(this.items[index].querySelector('.carousel-image'))
+        // this.items[this.currItem].classList.add('transparent')
+        this.items[this.currItem].querySelector('.carousel-image').classList.remove('opaque')
+        this.items[index].querySelector('.carousel-image').classList.add('opaque')
+        this.updateCarousel(index)
+        console.log(this)
+    }
+}
+document.querySelectorAll('.carousel').forEach( carousel => {
+    let carouselObj  = new Carousel(carousel);
+})
